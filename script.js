a3764d1b1db8d70236619fe0462420c594a58c14
marianalopesdev <52184708+marianalopesdev@users.noreply.github.com>
2022-08-24 02:04:13 -0300

    onclick feature added

--- script.js
+++ script.js
@@ -1,10 +1,11 @@
 function playSoundy(e) {
-    console.log(e)
+  
+  
  // const audio = e.keyCode;
     const audio = document.querySelector(`audio[data-key="${e.keyCode}"]`);
-   // console.log('audio'+audio);
+   console.log('audio '+audio);
     const key = document.querySelector(`div[data-key="${e.keyCode}"]`);
-
+    console.log('key '+key);
     if (!audio) return;
     
     key.classList.add('playing');
@@ -16,8 +17,31 @@ const keys = Array.from(document.querySelectorAll('.key'));
 keys.forEach(key => key.addEventListener('transitionend', removeTransition));
     
 window.addEventListener('keydown', playSoundy);
+window.addEventListener('onclick', playSoundy);
 
 function removeTransition(e) {
     if (e.propertyName !== 'transform') return;
     e.target.classList.remove('playing');
     }
+
+    function sayHello() {
+        console.log("Hello there !!");
+      }
+
+     
+     
+      function playSound() {
+        console.log(' iji')
+    
+      
+     // const audio = e.keyCode;
+        const audio = document.querySelector(`audio[data-key]`);
+       console.log('audio '+audio);
+        const key = document.querySelector(`div[data-key]`);
+        console.log('key '+key);
+        if (!audio) return;
+        
+        key.classList.add('playing');
+        audio.currentTime = 0;
+        audio.play();
+        }
