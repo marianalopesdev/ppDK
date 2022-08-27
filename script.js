c2deae03ab273463fe93a96641c274d6508948ae
marianalopesdev <52184708+marianalopesdev@users.noreply.github.com>
2022-08-27 14:23:14 -0300

    click function added

--- script.js
+++ script.js
@@ -1,47 +1,37 @@
-function playSoundy(e) {
-  
-  
- // const audio = e.keyCode;
+function playSoundKeyDown(e) {
+    console.log(e)
     const audio = document.querySelector(`audio[data-key="${e.keyCode}"]`);
-   console.log('audio '+audio);
     const key = document.querySelector(`div[data-key="${e.keyCode}"]`);
-    console.log('key '+key);
-    if (!audio) return;
-    
+  
+   if (!audio) return;
+   
     key.classList.add('playing');
     audio.currentTime = 0;
     audio.play();
-    }
+   }
 
-const keys = Array.from(document.querySelectorAll('.key'));
-keys.forEach(key => key.addEventListener('transitionend', removeTransition));
-    
-window.addEventListener('keydown', playSoundy);
-window.addEventListener('onclick', playSoundy);
 
 function removeTransition(e) {
     if (e.propertyName !== 'transform') return;
     e.target.classList.remove('playing');
-    }
+}
 
-    function sayHello() {
-        console.log("Hello there !!");
-      }
 
-     
-     
-      function playSound() {
-        console.log(' iji')
+const keys = Array.from(document.querySelectorAll('.key'));
+keys.forEach(key => key.addEventListener('transitionend', removeTransition));
+
+window.addEventListener('keydown', playSoundKeyDown);
+
+
+function playSoundOnClick(e) {
+  
+  const keyCode = e.dataset.key;
+  const audio = document.querySelector(`audio[data-key="${keyCode}"]`);
+  const key = document.querySelector(`div[data-key="${keyCode}"]`);
     
-      
-     // const audio = e.keyCode;
-        const audio = document.querySelector(`audio[data-key]`);
-       console.log('audio '+audio);
-        const key = document.querySelector(`div[data-key]`);
-        console.log('key '+key);
-        if (!audio) return;
-        
-        key.classList.add('playing');
-        audio.currentTime = 0;
-        audio.play();
-        }
+   if (!audio) return;
+   
+    key.classList.add('playing');
+    audio.currentTime = 0;
+    audio.play(); 
+   }
